9cc83a569f3c3af75eb998a05d20bb377c712be9
kimjh <kimjh@ubint.jp>
2020-04-11 11:44:21 +0900

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Kim Jong Hyun on 2020/04/11.
+//  Copyright © 2020 Kim Jong Hyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
