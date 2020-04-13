9b778503c6a006e7851d94a26bda373cee7a2cd5
surim <leesl@ubint.co.jp>
2020-04-13 17:37:11 +0900

    #キーボードーの設定

--- TestApp/ViewController/AddViewController.swift
+++ TestApp/ViewController/AddViewController.swift
@@ -18,7 +18,15 @@ class AddViewController: UIViewController {
     
     @IBOutlet weak private var message: UITextView!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var addBtn: UIButton!
+    
+    @IBOutlet weak var bottomConstraints: NSLayoutConstraint!
+    
     var addDelegate: AddDelegate?
+    
+    var saveOffset:CGFloat?
     
 //    var postViewModel: PostViewModel?
         
@@ -26,6 +34,16 @@ class AddViewController: UIViewController {
         super.viewDidLoad()
         print("AddViewController viewDidLoad")
         // Do any additional setup after loading the view.
+        
+        // Keyboard Notification Add
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Keyboard Notification Remove
+        NotificationCenter.default.removeObserver(UIResponder.keyboardWillShowNotification)
     }
 
     /*
@@ -60,5 +78,48 @@ class AddViewController: UIViewController {
         
         addDelegate?.postAdded(post: post)
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        print("show Keyboard")
+
+        guard let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        guard let btn = addBtn else { return }
+
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        let keyboardTopLine = view.frame.height - keyboardHeight
+
+        let bottomLine = btn.frame.origin.y + btn.frame.height
+        let displayBottom = bottomLine - scrollView.contentOffset.y
+
+        if displayBottom > keyboardTopLine {
+            let sub = displayBottom - keyboardTopLine
+            saveOffset = sub
+            let offset = CGPoint(x: scrollView.contentOffset.x, y: self.scrollView.contentOffset.y + sub)
+
+            scrollView.setContentOffset(offset, animated: true)
+            bottomConstraints.constant = keyboardHeight
+        }
+    }
+    
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+        print("Hide Keyboard")
+        
+        var offset:CGPoint = CGPoint(x: 0, y: 0)
+        if scrollView.contentOffset.y - (saveOffset ?? 0) < 0 {
+            offset = CGPoint(x: self.scrollView.contentOffset.x, y: 0)
+        }else if scrollView.contentOffset.y > (scrollView.contentSize.height - scrollView.frame.size.height - bottomConstraints.constant) {
+            offset = CGPoint(x: scrollView.contentOffset.x, y: scrollView.contentOffset.y)
+        }else {
+            offset = CGPoint(x: scrollView.contentOffset.x, y: self.scrollView.contentOffset.y - (saveOffset ?? 0))
+        }
+
+        saveOffset = nil
+        bottomConstraints.constant = 0
+        scrollView.setContentOffset(offset, animated: true)
+        
+        view.endEditing(true)
     }
 }
